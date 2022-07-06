7b8ce420dca33ff672414de55857dab3e7778579
Oleksandr26 <alemaevaleksandr96@gmail.com>
2022-07-06 18:50:02 +0300

    first task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+const onPlay = function (data) {
+    console.log(data);
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+} 
+
+player.on('timeupdate', onPlay);
+
+if (currentTime) {
+    timeupdate = throttle(1000);
+}
