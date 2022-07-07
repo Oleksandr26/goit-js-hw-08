d28739864a42bca90e25677640650c4de8935a54
Oleksandr26 <alemaevaleksandr96@gmail.com>
2022-07-07 15:00:32 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -10,8 +10,8 @@ const onPlay = function (data) {
     localStorage.setItem('videoplayer-current-time', data.seconds);
 } 
 
-player.on('timeupdate', onPlay);
+player.on('timeupdate', throttle(onPlay, 1000));
 
 if (currentTime) {
-    timeupdate = throttle(1000);
+    player.setCurrentTime(currentTime);
 }
